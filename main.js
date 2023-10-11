6f3be94dd98307b7f664b00e93981dd65b1a0706
Engy Mohamed <78613526+EngyMohamed-96@users.noreply.github.com>
2023-10-11 18:02:48 +0300

    add slider flies

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
 $(document).ready(function() {
+
+
   $(".navbar-icon").click(function() {
     $("#navbarLinks").toggleClass("responsive");
   });
+
+  var swiper = new Swiper('.mySwiper', {
+    slidesPerView: getSlidesPerView(),
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+
+  $(window).on('resize', function() {
+    swiper.params.slidesPerView = getSlidesPerView();
+    swiper.update();
+  });
+
+  function getSlidesPerView() {
+    var screenWidth = $(window).width();
+    if (screenWidth < 768) {
+      return 1.5;
+    } else if (screenWidth < 992) {
+      return 3.2;
+    } else if (screenWidth < 1200) {
+      return 3.2;
+    } else {
+      return 4.5;
+    }
+  }
 });
